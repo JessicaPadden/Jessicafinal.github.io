bc1644a42a9569415e758832b975fef35613ff5c
Jessica Padden <jessica.padden99@gmail.com>
2025-10-05 19:06:57 +0100

    mjs updat

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://JessicaPadden.github.io',
-  base: 'Jessicafinal.github.io',
+  base: '/Jessicafinal.github.io',
 })
 
