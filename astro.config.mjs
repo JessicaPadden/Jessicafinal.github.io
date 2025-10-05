1d92a31e5d98aacb43ab629f50c56af7ef81124e
Jessica Padden <jessica.padden99@gmail.com>
2025-10-05 20:00:20 +0100

    static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://JessicaPadden.github.io',
   base: '/Jessicafinal.github.io',
-})
+  output: 'static',
+});
 
